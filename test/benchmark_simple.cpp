3cfffb1aea65fe3590d0efef60fc2a8d9487cff7
Max Reichardt <mreichardt@gmx.net>
2013-07-06 11:53:38 +0200

    Added indentation to test program output in order to make it clearer.

--- test/benchmark_simple.cpp
+++ test/benchmark_simple.cpp
@@ -96,7 +96,7 @@ void BenchmarkCheapCopyPort()
 {
   tFrameworkElement* parent = new tFrameworkElement(&tRuntimeEnvironment::GetInstance(), "Test");
   tOutputPort<int> output_port("Output Port", parent, 32);
-  FINROC_LOG_PRINT(USER, "Port default value (expected 32): ", output_port.Get());
+  FINROC_LOG_PRINT(USER, "  Port default value (expected 32): ", output_port.Get());
   tInputPort<int> input_port("Input Port", parent);
   output_port.ConnectTo(input_port);
   parent->Init();
