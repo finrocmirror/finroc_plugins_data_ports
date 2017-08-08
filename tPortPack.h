532979ee46bbbba864cc6481e81ada92b919cdc4
Tobias Föhst <foehst@finroc.org>
2017-08-08 16:30:38 +0200

    Backed out last changes due to incompatibility with gcc 4.8.4 on Ubuntu 14.04 - Backed out changeset 4756cdcbb205 - Backed out changeset e4c5d0d9de8c

--- tPortPack.h
+++ tPortPack.h
@@ -76,9 +76,6 @@ template <template <typename> class TPort, typename TTypeList, size_t Tsize = rr
 class tPortPack : private tPortPack < TPort, TTypeList, Tsize - 1 >
 {
 
-  template <bool value, typename>
-  using CheckIterators = std::integral_constant<bool, value>;
-
 //----------------------------------------------------------------------
 // Public methods and typedefs
 //----------------------------------------------------------------------
@@ -91,18 +88,10 @@ public:
     this->port.Init();
   }
 
-  template <typename TIterator, typename std::enable_if<CheckIterators<(Tsize> 1), TIterator>::value, int>::type = 0 >
-      inline tPortPack(core::tFrameworkElement *parent, TIterator names_begin, TIterator names_end) :
-        tPortPack < TPort, TTypeList, Tsize - 1 > (parent, names_begin, names_end - 1),
-        port(*(names_end - 1), parent)
-  {
-    this->port.Init();
-  }
-
-  template <typename TIterator, typename std::enable_if<CheckIterators<(Tsize == 1), TIterator>::value, int>::type = 0>
+  template <typename TIterator>
   inline tPortPack(core::tFrameworkElement *parent, TIterator names_begin, TIterator names_end) :
-    tPortPack < TPort, TTypeList, Tsize - 1 > (parent, *names_begin),
-    port(*names_begin, parent)
+    tPortPack < TPort, TTypeList, Tsize - 1 > (parent, names_begin, names_end - 1),
+    port(names_end - 1, parent)
   {
     this->port.Init();
   }
@@ -144,11 +133,6 @@ struct tPortPack <TPort, TTypeList, 0>
 {
   inline tPortPack(core::tFrameworkElement *parent, const std::string &name_prefix)
   {}
-
-  template <typename TIterator>
-  inline tPortPack(core::tFrameworkElement *parent, TIterator names_begin, TIterator names_end)
-  {}
-
   inline core::tPortWrapperBase &GetPort(size_t index)
   {
     return *reinterpret_cast<core::tPortWrapperBase *>(0);
